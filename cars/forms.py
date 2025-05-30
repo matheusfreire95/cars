48d7f07c40d718a2d11f4ccc9dbef4169712848f
Matheus Freire <ffreirematheus@gmail.com>
2025-05-30 17:51:26 -0300

    adiciona cadastro carros

--- cars/forms.py
+++ cars/forms.py
@@ -1,7 +1,27 @@
 from django import forms
+from .models import Brand, Car
 
-from .models import Brand
 
 class CarForm(forms.Form):
-    model = forms.CharField()
-    brand = forms.ChoiceField(Brand.objects.all())
+    model = forms.CharField(max_length=200)
+    brand = forms.ModelChoiceField(queryset=Brand.objects.all())
+    factory_year = forms.IntegerField()
+    model_year = forms.IntegerField()
+    plate = forms.CharField(max_length=10)
+    value = forms.DecimalField()
+    photo = forms.ImageField()
+
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.cleaned_data['brand'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_year = self.cleaned_data['model_year'],
+            plate = self.cleaned_data['plate'],
+            value = self.cleaned_data['value'],
+            photo = self.cleaned_data['photo'],
+        )
+        
+        car.save()
+        
+        return car
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from .models import Car
+from cars.forms import CarForm
+
 
 def cars_view(request):
     cars = Car.objects.all().order_by('model')
@@ -12,4 +13,14 @@ def cars_view(request):
     return render(request, "cars.html", {"cars": cars})
 
 def new_car_view(request):
-    return render(request, "new_car.html")
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+
+        if new_car_form.is_valid():
+            new_car_form.save()
+
+            return redirect('cars_list')
+    else:
+        new_car_form = CarForm()
+
+    return render(request, "new_car.html", {'new_car_form': new_car_form})
