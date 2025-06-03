bc593656541a661436cb1609b2383ff5d9939187
Matheus Freire <ffreirematheus@gmail.com>
2025-06-03 17:25:12 -0300

    adiciona app accounts

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+
+def register_view(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST, request.FILES)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            return redirect('login')
+    else:
+        user_form = UserCreationForm()
+
+    return render(request, 'register.html', {'user_form': user_form})
+
+
+    
