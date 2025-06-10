48e0f3cbcad8a59ef82c37ddfbff3d03104a4064
Matheus Freire <ffreirematheus@gmail.com>
2025-06-10 12:57:01 -0300

    adiciona model inventario

--- cars/models.py
+++ cars/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+class Inventory(models.Model):
+    car_quantity = models.IntegerField()
+    car_value = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.car_quantity} cars - ${self.car_value:.2f}' if self.car_value is not None else f'{self.car_quantity} cars'
+
+
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
