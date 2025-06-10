749a7ebfba6fd3bd515d87dec769f7ecf081ca44
Matheus Freire <ffreirematheus@gmail.com>
2025-06-09 21:23:17 -0300

    adiciona melhorias carros

--- cars/views.py
+++ cars/views.py
@@ -1,32 +1,26 @@
-from django.shortcuts import render, redirect
-from django.views import View
 from .models import Car
 from cars.forms import CarModelForm
+from django.views.generic import ListView, CreateView
 
 
-class CarsView(View):
-    def get(self, request):
-        cars = Car.objects.all().order_by('model')
-        search = request.GET.get('search')
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
 
         if search:
-            cars = Car.objects.filter(model__icontains=search)
+            cars = cars.filter(model__icontains=search)
+        
+        return cars
 
-        return render(request, "cars.html", {"cars": cars})
 
+class CarsCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/'
 
-class NewCarView(View):
-    def get(self, request):
-        new_car_form = CarModelForm()
-
-        return render(request, "new_car.html", {'new_car_form': new_car_form})
-    
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-
-        if new_car_form.is_valid():
-            new_car_form.save()
-
-            return redirect('cars_list')
-
-        return render(request, "new_car.html", {'new_car_form': new_car_form})
